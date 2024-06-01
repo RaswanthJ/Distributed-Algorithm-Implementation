cf9775e3bec8043f4ee98a831361ed6788b83ac4
RaswanthJ <97757031+RaswanthJ@users.noreply.github.com>
2024-06-02 03:00:58 +0530

    Add files via upload

---
+++ Consensus/Three Node Consensus with Coordinator/Coordinator.py
@@ -0,0 +1,33 @@
+import socket
+import json
+
+def send_init_message(ip, port,iteration_number,neighbors,alpha,iter,nodes):
+    message = {
+        "init": "INIT",
+        "inum": iteration_number,
+        "alpha": alpha,
+        "iter": iter,
+        "neighbors": neighbors,
+        "nodes": nodes,
+        "num_nodes": num_nodes
+    }
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.sendto(json.dumps(message).encode('utf-8'), (ip, port))
+
+
+if __name__ == "__main__":
+    nodes = [("127.0.0.1", 8001), ("127.0.0.1", 8002), ("127.0.0.1", 8003)]
+    iteration_number = 50
+    neighbors = [
+        [],
+        [2,3],
+        [1,3],
+        [1,2]
+    ]
+    num_nodes = 5
+    alpha = 0.1
+    iter = 1
+    for ip, port in nodes:
+        send_init_message(ip, port,iteration_number,neighbors,alpha,iter,nodes)
+
+    print("Initialization messages sent to all nodes.")
