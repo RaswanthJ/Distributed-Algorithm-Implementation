a43a85350cfcb43496911145f4b90cd64c0fa0da
RaswanthJ <97757031+RaswanthJ@users.noreply.github.com>
2024-06-26 10:33:28 +0530

    Update Add.py

--- Remote Access/Add.py
+++ Remote Access/Add.py
@@ -1,3 +1,21 @@
+import os
+
 a = 10
 b = 20
-print(a+b)
+
+pth = "/home/rasp3/Desktop/Test/n.txt"
+
+with open(pth,'w') as f:
+    f.write(str(a+b))
+
+print("File Created")
+
+ip = "169.254.253.114"
+port = 22
+user = "rasp2"
+
+n_script = "n.txt"
+
+scp_command = f"scp -P {port} {n_script} {user}@{ip}:/home/{user}/Desktop/Remote Access Test/"
+print(f"Transferring {n_script} to {user}@{ip}...")
+os.system(scp_command)
