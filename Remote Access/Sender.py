661fb4a706c5f26a36f7399df578d70defbb7d49
RaswanthJ <97757031+RaswanthJ@users.noreply.github.com>
2024-06-26 10:33:47 +0530

    Update Sender.py

--- Remote Access/Sender.py
+++ Remote Access/Sender.py
@@ -1,15 +1,15 @@
 import os
 import subprocess
 
-ip = 
+ip = "169.254.142.206"
 port = 22
-user = 
+user = "rasp3"
 node_script = "Add.py"
 
-scp_command = f"scp -P {port} {node_script} {user}@{ip}:/home/{user}/"
+scp_command = f"scp -P {port} {node_script} {user}@{ip}:/home/{user}/Desktop/Test/"
 print(f"Transferring {node_script} to {user}@{ip}...")
 os.system(scp_command)
 
-ssh_command = f"ssh -p {port} {user}@{ip} 'python3 /home/{user}/{node_script}'"
+ssh_command = f"ssh -p {port} {user}@{ip} 'python3 /home/{user}/Desktop/Test/{node_script}'"
 print(f"Running {node_script} on {user}@{ip}...")
 subprocess.run(ssh_command, shell=True)
